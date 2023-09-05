d2fd21b12c0aa3338d46241293125799cb827b06
DazviL <112430049+DazviL@users.noreply.github.com>
2023-09-05 12:23:30 -0300

    Add files via upload

---
+++ semana2.js
@@ -0,0 +1,18 @@
+let animal = "cachorro";
+
+switch (animal) {
+    case "cachorro":
+        console.log("Latindo");
+        break;
+    case "gato":
+        console.log("Miando");
+        break;
+    case "vaca":
+        console.log("Mugindo");
+        break;
+    case "galinha":
+        console.log("Cocoricando");
+        break;
+    default:
+        console.log("Não sei que som esse animal faz");
+}
---
+++ semana3.js
@@ -0,0 +1,18 @@
+let pontuacao = 85;
+
+switch (true) {
+    case pontuacao >= 90:
+        console.log("Conceito A");
+        break;
+    case pontuacao >= 80:
+        console.log("Conceito B");
+        break;
+    case pontuacao >= 70:
+        console.log("Conceito C");
+        break;
+    case pontuacao >= 60:
+        console.log("Conceito D");
+        break;
+    default:
+        console.log("Conceito F");
+    }
